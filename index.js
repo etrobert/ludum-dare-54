2f24eaf5146367268c6f11cf461888e6fa9ffb54
erobert <etienne.robert@doctolib.com>
2023-09-11 19:43:33 +0200

    Add a canvas

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Hello World!');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'green';
+// Add a rectangle at (10, 10) with size 100x100 pixels
+ctx.fillRect(10, 10, 100, 100);
