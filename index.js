64d13a76b096bc1679dafe31739b5056e9112a05
erobert <etienne.robert@doctolib.com>
2023-09-13 21:05:03 +0200

    Create range and combination functions

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ const gridSize = {
 
 const cellSize = 40; // px
 
-for (let y = 0; y < gridSize.height; ++y) {
-  for (let x = 0; x < gridSize.width; ++x) {
-    ctx.fillRect(x * (cellSize + 1), y * (cellSize + 1), cellSize, cellSize);
-  }
-}
+/**
+ * @returns an array of consecutive integers from 0 to n-1
+ * @example range(4) // [0, 1, 2, 3]
+ */
+const range = (n) => Array.from(Array(n).keys());
+
+const combination = (a1, a2) => a1.flatMap((e1) => a2.map((e2) => [e1, e2]));
+
+/**
+ * @returns The combination of two ranges n1 and n2
+ * @example range2d(2, 2) // [[0, 0], [0, 1], [1, 0], [1, 1]]
+ */
+const range2d = (n1, n2) => combination(range(n1), range(n2));
+
+range2d(gridSize.width, gridSize.height).forEach(([x, y]) =>
+  ctx.fillRect(x * (cellSize + 1), y * (cellSize + 1), cellSize, cellSize)
+);
