e77556693475b9b858d36d9e7ee71e326dbb59c0
erobert <etienne.robert@doctolib.com>
2023-09-13 20:47:28 +0200

    Display the grid

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'green';
-// Add a rectangle at (10, 10) with size 100x100 pixels
-ctx.fillRect(10, 10, 100, 100);
+
+const gridSize = {
+  width: 3,
+  height: 3,
+};
+
+const cellSize = 40; // px
+
+for (let y = 0; y < gridSize.height; ++y) {
+  for (let x = 0; x < gridSize.width; ++x) {
+    ctx.fillRect(x * (cellSize + 1), y * (cellSize + 1), cellSize, cellSize);
+  }
+}
