5bcafcafe85ffcbdb078f7a6f5f09ccdaeb60534
erobert <etienne.robert@doctolib.com>
2023-09-22 16:46:38 +0200

    Add acceleration

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const gravity = {
+  x: 0,
+  y: 1 / 1000 / 100,
+};
+
 const character = {
   position: {
     x: 10,
@@ -8,8 +13,9 @@ const character = {
   },
   speed: {
     x: 0, // px per ms
-    y: 10 / 1000, // px per ms
+    y: 0, // px per ms
   },
+  acceleration: gravity,
   size: {
     x: 20,
     y: 20,
@@ -52,8 +58,21 @@ const collision = (entity1, entity2) =>
   entity1.position.y < entity2.position.y + entity2.size.y &&
   entity1.position.y + entity1.size.y > entity2.position.y;
 
+const updateSpeed = (entity, timeDelta) =>
+  entity.acceleration
+    ? {
+        ...entity,
+        speed: {
+          x: entity.speed.x + entity.acceleration.x * timeDelta,
+          y: entity.speed.y + entity.acceleration.y * timeDelta,
+        },
+      }
+    : entity;
+
 const updateState = (state, timeDelta) =>
   state.map((entity, index) => {
+    entity = updateSpeed(entity, timeDelta);
+
     const newPosition = {
       x: entity.position.x + entity.speed.x * timeDelta,
       y: entity.position.y + entity.speed.y * timeDelta,
