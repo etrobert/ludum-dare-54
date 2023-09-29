cf946f2ec656d8430e794f0d4fd264378829f1bf
erobert <etienne.robert@doctolib.com>
2023-09-29 07:26:27 +0200

    Extract level in squareLevel

--- index.js
+++ index.js
@@ -1,44 +1,8 @@
 import render from './render.js';
 import updateState from './updateState.js';
+import squareLevel from './squareLevel.js';
 
-const gravity = {
-  x: 0,
-  y: 1 / 1000 / 20,
-};
-
-const character = {
-  name: 'character',
-  position: { x: 30, y: 30 },
-  speed: { x: 0, y: 0 }, // px per ms
-  acceleration: gravity,
-  size: { x: 20, y: 20 },
-};
-
-const platform1 = {
-  name: 'platform1',
-  position: { x: 10, y: 100 },
-  size: { x: 100, y: 10 },
-};
-
-const platform2 = {
-  name: 'platform2',
-  position: { x: 100, y: 10 },
-  size: { x: 10, y: 100 },
-};
-
-const platform3 = {
-  name: 'platform3',
-  position: { x: 10, y: 10 },
-  size: { x: 10, y: 100 },
-};
-
-const platform4 = {
-  name: 'platform4',
-  position: { x: 10, y: 10 },
-  size: { x: 100, y: 10 },
-};
-
-let state = [character, platform1, platform2, platform3, platform4];
+let state = squareLevel;
 
 let previousTime = Date.now();
 
