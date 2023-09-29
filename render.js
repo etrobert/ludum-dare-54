5f69b16a92ad09044cac3c7418cdfd6a72b50c17
erobert <etienne.robert@doctolib.com>
2023-09-29 08:00:31 +0200

    Increase game scale

--- render.js
+++ render.js
@@ -1,15 +1,17 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const scale = 2.5;
+
 const render = (state) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = 'green';
   state.map((entity) =>
     ctx.fillRect(
-      entity.position.x,
-      entity.position.y,
-      entity.size.x,
-      entity.size.y
+      entity.position.x * scale,
+      entity.position.y * scale,
+      entity.size.x * scale,
+      entity.size.y * scale
     )
   );
 };
