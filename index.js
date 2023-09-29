c40cebaf97f13edfdfcbfea16c329bb9937e88ce
erobert <etienne.robert@doctolib.com>
2023-09-29 07:44:47 +0200

    Add user controls

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ let state = squareLevel;
 
 let previousTime = Date.now();
 
+const getUserAcceleration = () => {
+  if (controls.ArrowLeft && controls.ArrowRight) return 0;
+  if (controls.ArrowRight) return 0.1 / 1000;
+  if (controls.ArrowLeft) return -0.1 / 1000;
+  return 0;
+};
+
 const gameLoop = () => {
   const currentTime = Date.now();
   const timeDelta = currentTime - previousTime;
   previousTime = currentTime;
 
+  state[0].acceleration.x = getUserAcceleration(); // TODO: Preserve existing acceleration
   state = updateState(state, timeDelta);
 
   render(state);
 };
+
+let controls = {};
+
+document.addEventListener('keydown', (event) => (controls[event.key] = true));
+document.addEventListener('keyup', (event) => (controls[event.key] = false));
 
 setInterval(gameLoop, 30);
