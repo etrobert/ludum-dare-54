a94c64052e4bcffd09f2ebf59bd63aa86789fda7
erobert <etienne.robert@doctolib.com>
2023-09-29 21:38:49 +0200

    Display the jellyfish

--- render.js
+++ render.js
@@ -1,19 +1,36 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+ctx.imageSmoothingEnabled = false;
 
 const scale = 2.5;
+
+const jellyfishImage = new Image();
+jellyfishImage.src = 'jellyfish.png';
 
 const render = (state) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = 'green';
-  state.map((entity) =>
-    ctx.fillRect(
-      entity.position.x * scale,
-      entity.position.y * scale,
-      entity.size.x * scale,
-      entity.size.y * scale
-    )
-  );
+  state.map((entity, index) => {
+    if (index === 0)
+      ctx.drawImage(
+        jellyfishImage,
+        0,
+        0,
+        16,
+        16,
+        entity.position.x * scale,
+        entity.position.y * scale,
+        entity.size.x * scale,
+        entity.size.y * scale
+      );
+    else
+      ctx.fillRect(
+        entity.position.x * scale,
+        entity.position.y * scale,
+        entity.size.x * scale,
+        entity.size.y * scale
+      );
+  });
 };
 
 export default render;
