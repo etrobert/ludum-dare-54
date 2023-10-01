6b22a60f0a1b29d5c0ed1512ab94f0548f2bea29
Palomia <amaury.palomino@outlook.com>
2023-10-01 21:23:29 +0200

    Update sounds so they can have other volumes

--- public/audio/openSounds.js
+++ public/audio/openSounds.js
@@ -7,14 +7,17 @@ trackJumpSfx.connect(AudioCtx.destination);
 const dashSfx = document.querySelector('#dash-sfx');
 const trackdashSfx = AudioCtx.createMediaElementSource(dashSfx);
 trackdashSfx.connect(AudioCtx.destination);
+dashSfx.volume = 1;
 
 const enemyDeath = document.querySelector('#enemy-death-sfx');
 const trackenemyDeathSfx = AudioCtx.createMediaElementSource(enemyDeath);
 trackenemyDeathSfx.connect(AudioCtx.destination);
+enemyDeath.volume = 1;
 
 const lossHealthSfx = document.querySelector('#loss-health-sfx');
 const tracklossHealthSfx = AudioCtx.createMediaElementSource(lossHealthSfx);
 tracklossHealthSfx.connect(AudioCtx.destination);
+lossHealthSfx.volume = 1;
 
 const shroudMusic = document.querySelector('#shroud-music');
 const trackshroudMusic = AudioCtx.createMediaElementSource(shroudMusic);
@@ -23,10 +26,12 @@ trackshroudMusic.connect(AudioCtx.destination);
 const gameMusic = document.querySelector('#game-music');
 const gameMusicTrack = AudioCtx.createMediaElementSource(gameMusic);
 gameMusicTrack.connect(AudioCtx.destination);
+gameMusic.volume = 1;
 
 const startMusic = document.querySelector('#start-music');
 const startMusicTrack = AudioCtx.createMediaElementSource(startMusic);
 startMusicTrack.connect(AudioCtx.destination);
+startMusic.volume = 1;
 
 export {
   AudioCtx,
